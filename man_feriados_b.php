90179683095bb7190e335f6b1a4c735ec66fbda4
Demoniacchl <garridovice@gmail.com>
2024-04-15 15:50:15 -0400

    v.1.7.4

--- man_feriados_b.php
+++ man_feriados_b.php
@@ -1,16 +1,12 @@
 <?php
 session_start();
 error_reporting(1);
-
 use Database\Conexion;
 require_once "Database.php";
-
 $db = new Conexion();
 $link = $db->getLink();
-
 if (isset($_POST['id'])) {
     $idRegistro = $_POST['id'];
-    
     // Preparar la sentencia SQL
     $stmt = $link->prepare("DELETE FROM gd_feriados WHERE fer_id = ?");
     if (!$stmt) {
@@ -19,7 +15,6 @@ if (isset($_POST['id'])) {
     }
     // Vincular los parámetros
     $stmt->bind_param("i", $idRegistro);
-
     // Ejecutar la sentencia
     if ($stmt->execute()) {
         echo json_encode(['error' => false, 'message' => "Datos Borrados correctamente"]);
